fb35f211f64311faaaa52fd0fa41ebd0420d77ec
Pedro Urbina <purbina14@gmail.com>
2020-05-01 19:34:45 -0400

    Completed

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,63 +1,76 @@
 import java.util.Arrays;
 
 public class MergeSort {
-    public static void main(String[] args) {
-        int[] array = {9, 3, 4, 2, 1, -7, 0, 5};
-        System.out.println(Arrays.toString(mergeSort(array)));
-        System.out.println("Steps taken: " + steps);
-    }
 
     static int steps = 0;
 
-    public static int[] mergeSort(int [] array) {
-        // if array has less than 2 elements, return as is
-        if (array.length < 2) {
-            return array;
+    public static void main(String[] args) {
+        int[] array = {9, 3, 4, 2, 1, -7, 0, 5};
+
+        System.out.print("Input array: ");
+        System.out.println(Arrays.toString(array));
+
+        MergeSort input = new MergeSort();
+        input.sort(array, 0, array.length - 1);
+
+        System.out.print("Sorted array: ");
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("While-loop iterations: " + steps);
+    }
+
+    void sort(int[] array, int startingIndex, int endingIndex) {
+        if (startingIndex < endingIndex) { // only sort if there is more than one element
+            int middleIndex = (startingIndex + endingIndex) / 2;
+
+            sort(array, startingIndex, middleIndex); // sort left half
+            sort(array, middleIndex + 1, endingIndex); // sort right half
+
+            merge(array, startingIndex, middleIndex, endingIndex); // merge sorted halves
         }
+    }
 
-        // split array into two halves
-        int n = array.length;
-        int[] leftHalf = new int[n / 2];
-        int[] rightHalf = new int[n - leftHalf.length];
-        int nLeft = leftHalf.length;
-        int nRight = rightHalf.length;
+    void merge(int[] array, int startingIndex, int middleIndex, int endingIndex) {
+        // find lengths of sub arrays
+        int n1 = middleIndex - startingIndex + 1;
+        int n2 = endingIndex - middleIndex;
 
-        System.arraycopy(array, 0, leftHalf, 0, nLeft);
-        System.arraycopy(array, nLeft, rightHalf, 0, nRight);
+        // create sub arrays and copy values from main array
+        int[] leftHalf = new int[n1];
+        int[] rightHalf = new int[n2];
 
-        // sort, merge, and return arrays
-        int[] sortedArray = new int[n];
+        System.arraycopy(array, startingIndex, leftHalf, 0, n1);
+        System.arraycopy(array, middleIndex + 1, rightHalf, 0, n2);
+
+        // merge sub arrays back into main array in proper order
         int i = 0;
         int j = 0;
-        int k = 0;
+        int k = startingIndex;
 
-        while (i < n) {
-            if (j == nLeft) {
-                sortedArray[i] = mergeSort(rightHalf)[k];
-                k++;
+        while (i < n1 && j < n2) {
+            if (leftHalf[i] <= rightHalf[j]) {
+                array[k] = leftHalf[i];
                 i++;
-                continue;
-            } else if (k == nRight) {
-                sortedArray[i] = mergeSort(leftHalf)[j];
-                j++;
-                i++;
-                continue;
             } else {
-                int nextElementLeft = mergeSort(leftHalf)[j];
-                int nextElementRight = mergeSort(rightHalf)[k];
-
-                if (nextElementLeft <= nextElementRight) {
-                    sortedArray[i] = nextElementLeft;
-                    j++;
-                } else {
-                    sortedArray[i] = nextElementRight;
-                    k++;
-                }
-                i++;
+                array[k] = rightHalf[j];
+                j++;
             }
+            k++;
             steps++;
         }
-        System.out.println(Arrays.toString(sortedArray));
-        return sortedArray;
+
+        while (i < n1) {
+            array[k] = leftHalf[i];
+            i++;
+            k++;
+            steps++;
+        }
+
+        while (j < n2) {
+            array[k] = rightHalf[j];
+            j++;
+            k++;
+            steps++;
+        }
     }
 }
